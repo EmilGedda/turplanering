96eb12f344cdc18a49fee9cb3d74a5c118478f06
Emil Gedda <emil.gedda@emilgedda.se>
2020-08-08 00:20:02 +0200

    add initial input for search and working center on gps button

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,8 +1,96 @@
 import React from 'react'
-import TrailMap from './map/TrailMap'
+import { useState } from 'react'
+import { Map } from 'react-leaflet'
+import {
+    TextField,
+    Box,
+    InputAdornment,
+    Button,
+    makeStyles,
+} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import GpsFixedIcon from '@material-ui/icons/GpsFixed'
+import BufferedTileLayer from './map/BufferedTileLayer'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    margin: {
+        margin: theme.spacing(1),
+    },
+    textSize: {
+        fontSize: '15pt',
+    },
+}))
 
 const App: React.FC = () => {
-    return <TrailMap />
+    const classes = useStyles()
+
+    const [viewport, setViewport] = useState({
+        center: [59.334591, 18.06324] as [number, number],
+        zoom: 8,
+    })
+
+    const centerToGPS = () => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            const { latitude, longitude } = pos.coords
+            console.log(pos)
+            setViewport({
+                center: [latitude, longitude],
+                zoom: 13,
+            })
+        })
+    }
+    return (
+        <div>
+            <Box
+                bgcolor="white"
+                borderRadius={4}
+                style={{
+                    border: '2px solid #ccc',
+                    display: 'inline-block',
+                    margin: '50px',
+                }}
+            >
+                <TextField
+                    type="search"
+                    placeholder="Sök"
+                    style={{
+                        margin: '0px',
+                    }}
+                    InputProps={{
+                        classes: {
+                            input: classes.textSize,
+                        },
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon
+                                    style={{
+                                        color: 'rgb(120, 120, 120)',
+                                        marginTop: '-1px',
+                                        marginLeft: '5px',
+                                    }}
+                                />
+                            </InputAdornment>
+                        ),
+                        disableUnderline: true,
+                    }}
+                />
+            </Box>
+            <Button variant="contained" size="small" onClick={centerToGPS}>
+                <GpsFixedIcon />
+            </Button>
+            <Map id="trailmap" viewport={viewport} useFlyTo={true}>
+                <BufferedTileLayer
+                    url="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    bufferRadius={2}
+                />
+            </Map>
+        </div>
+    )
 }
 
 export default App
--- web/src/components/map/BufferedTileLayer.tsx
+++ web/src/components/map/BufferedTileLayer.tsx
@@ -1,25 +1,24 @@
 import { TileLayer, Bounds, point } from 'leaflet'
-import {
-    withLeaflet,
-    GridLayer,
-    GridLayerProps,
-    LeafletContext,
-} from 'react-leaflet'
+import { withLeaflet, GridLayer, GridLayerProps } from 'react-leaflet'
 
-type Props = {
-    url: string
-    leaflet: LeafletContext
-    bufferRadius?: number
-} & GridLayerProps
+type Props = { url: string; bufferRadius?: number } & GridLayerProps
 
 class BufferedTileLayer extends GridLayer<Props, TileLayer> {
+    updateLeafletElement(fromProps: Props, toProps: Props) {
+        super.updateLeafletElement(fromProps, toProps)
+        if (toProps.url !== fromProps.url) {
+            this.leafletElement.setUrl(toProps.url)
+        }
+    }
+
     createLeafletElement(props: Props): TileLayer {
+        const getRadius = () => this.props.bufferRadius || 2
         return new (class extends TileLayer {
             _pxBoundsToTileRange(pixels: Bounds): Bounds {
                 /* eslint-disable */
                 // @ts-ignore: Hack to wrap undeclared function
                 const viewport = super._pxBoundsToTileRange(pixels) as Bounds,
-                    padding = props.bufferRadius || 2,
+                    padding = Math.max(getRadius(), 0),
                     margin = point(padding, padding)
                 return new Bounds(
                     viewport.min!.subtract(margin),
