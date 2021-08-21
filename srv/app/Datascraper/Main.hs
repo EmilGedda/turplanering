b0e72595937887959ff6c31f993bd28f8dd4a410
Emil Gedda <emil.gedda@emilgedda.se>
2021-08-21 17:50:06 +0200

    srv: run fourmolu

--- srv/app/Datascraper/Main.hs
+++ srv/app/Datascraper/Main.hs
@@ -1,26 +1,26 @@
 module Main where
 
-import Control.Exception
-import Data.Aeson
-import Data.Function
-import Data.Geospatial
-import Network.Curl
-import qualified Data.Sequence as Seq
-import qualified Streamly.Prelude as Streamly
-
+import           Control.Exception
+import           Data.Aeson
+import           Data.Function
+import           Data.Geospatial
+import           Network.Curl
+import qualified Data.Sequence     as Seq
+import qualified Streamly.Prelude  as Streamly
 
 naturkartan :: [(Int, String)]
-naturkartan = zip [1..] $ map baseUrl [1..31]
-    where baseUrl n = "https://api.naturkartan.se/v2/sites/search.geojson"
-                    ++ "?search%5Bguide_ids%5D%5B%5D="
-                    ++ show n ++ "&search%5Bimportance%5D=3"
-
+naturkartan = zip [1 ..] $ map baseUrl [1 .. 31]
+    where
+        baseUrl n =
+            "https://api.naturkartan.se/v2/sites/search.geojson"
+                ++ "?search%5Bguide_ids%5D%5B%5D="
+                ++ show n
+                ++ "&search%5Bimportance%5D=3"
 
 type GeoJSON = GeoFeatureCollection Value
 
-
 data ScrapingException = DecodeError Int String
-    deriving Show
+    deriving (Show)
 
 instance Exception ScrapingException
 
@@ -30,25 +30,28 @@ fetch n url = do
     putStr "Fetched geojson #"
     print n
     case eitherDecode body of
-      Left msg -> throwIO $ DecodeError n msg
-      Right geojson -> return geojson
+        Left msg -> throwIO $ DecodeError n msg
+        Right geojson -> return geojson
 
 mergeGeoJSON :: GeoJSON -> GeoJSON -> GeoJSON
-mergeGeoJSON a b = GeoFeatureCollection Nothing
-    $ _geofeatures a Seq.>< _geofeatures b
-
+mergeGeoJSON a b =
+    GeoFeatureCollection Nothing $
+        _geofeatures a Seq.>< _geofeatures b
 
 serialize :: ToJSON a => a -> IO ()
-serialize json = encodeFile file json
-         *> putStr "Saved data in "
-         *> putStrLn file
-    where file = "naturkartan.geojson"
+serialize json =
+    encodeFile file json
+        *> putStr "Saved data in "
+        *> putStrLn file
+    where
+        file = "naturkartan.geojson"
 
 main :: IO ()
-main = withCurlDo
-        $ Streamly.fromList naturkartan
-        & Streamly.fromSerial
-        & Streamly.mapM (uncurry fetch)
-        & Streamly.fromAsync
-        & Streamly.foldr mergeGeoJSON  (GeoFeatureCollection Nothing Seq.empty)
-        >>= serialize
+main =
+    withCurlDo $
+        Streamly.fromList naturkartan
+            & Streamly.fromSerial
+            & Streamly.mapM (uncurry fetch)
+            & Streamly.fromAsync
+            & Streamly.foldr mergeGeoJSON (GeoFeatureCollection Nothing Seq.empty)
+            >>= serialize
